fb3f02e2d70ed6aece8aebeeb4cc40297c0a213e
Prajna396 <prajnasree396@gmail.com>
2026-05-06 20:18:06 +0530

    Add SQL practice queries for Employee and Department

---
+++ Week-01/Day-01/practice_queries.sql
@@ -0,0 +1,15 @@
+SELECT * FROM Employee;
+
+SELECT name, salary 
+FROM Employee;
+
+SELECT * 
+FROM Employee
+WHERE age > 30;
+
+SELECT name 
+FROM Department;
+
+SELECT * 
+FROM Employee
+WHERE department_id = 1;
